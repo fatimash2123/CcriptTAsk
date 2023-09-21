73b567af1aa4f82912949a0943b762181fddf93d
fatimash2123 <fshahzad2123@gmail.com>
2023-09-21 20:34:58 +0500

    appointment return fixed

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -5,15 +5,17 @@ const appointments = [
         reason: "Lorem Ipsum",
         day: "monday",
         start: "8 AM",
-        end: "10 AM"
+        end: "10 AM",
+        totalTime: 3
     },
     {
         reason: "Lorem Ipsum",
         day: "wednesday",
         start: "11 AM",
-        end: "12 PM"
+        end: "12 PM",
+        totalTime: 2
     }
-];
+]
 const getAllappointments = asyncHandler((req, res) => {
     res.status(200).json({ user: req.user.username, appointments })
 })
